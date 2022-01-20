c72ba28d2765a4bf619194d8b2269ac873abed56
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-20 22:46:15 +0530

    Update stream.py

--- plugins/stream.py
+++ plugins/stream.py
@@ -13,7 +13,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 
 
-@Client.on_message(filters.private & (filters.media | filters.text))
+@Client.on_message(filters.private & (filters.media | filters.text | filters.command(["stream"]))
 async def filter(bot, update):
     if update.text:
         if not update.text.startswith("http://") or not update.text.startswith("https://"):
