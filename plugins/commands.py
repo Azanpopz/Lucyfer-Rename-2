b67e8ca7139bb4b4e19b5e5b2867d7c5a6d78be7
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-21 19:33:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -105,12 +105,7 @@ async def start(client, message):
             f_caption = f_caption + f"\n\n<code>┈•••✿</code>😄😄😄<code>✿•••┈</code>"
             i += 1
             try:
-                    k = await message.reply(f"⏳DOWNLOADING⏳◎ ◎")
-                    await asyncio.sleep(1)
-                    await k.delete()
-                    k = await message.reply(f"⏳DOWNLOADING⏳◎ ◎ ◎ ◎")
-                    await asyncio.sleep(1)
-                    await k.delete()
+                    
                     
                     k = await client.send_cached_media(
                     chat_id=message.from_user.id,
