241b5f08054f7c2735576abaa671db23580e3780
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-21 13:01:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -99,7 +99,7 @@ async def start(client, message):
         await temp_msg.delete()
     if CUSTOM_FILE_CAPTION:
         try:
-            f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+            f_caption=CUSTOM_FILE_CAPTION.format(file_size=size, file_caption=f_caption)
         except Exception as e:
             logger.exception(e)
             f_caption=f_caption
