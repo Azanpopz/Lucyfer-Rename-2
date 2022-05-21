5a671e3606b8c6d4c03d426386bee8d4ca9a453a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-21 20:06:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -118,24 +118,24 @@ async def start(client, message):
             try:
 
                     files = files_[0]
-    title = files.file_name
-    size=get_size(files.file_size)
-    f_caption=files.caption
-    if CUSTOM_FILE_CAPTION:
-        try:
-            f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
-        except Exception as e:
-            logger.exception(e)
-            f_caption=files.caption
-    if CUSTOM_FILE_CAPTION:
-        try:
-            f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
-        except Exception as e:
-            logger.exception(e)
-            f_caption=f_caption
-    if f_caption is None:
-        f_caption = f"{files.file_name}"
-
+                    title = files.file_name
+                    size=get_size(files.file_size)
+                    f_caption=files.caption
+                    if CUSTOM_FILE_CAPTION:
+                        try:
+                            f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                        except Exception as e:
+                            logger.exception(e)
+                            f_caption=files.caption
+                    if CUSTOM_FILE_CAPTION:
+                        try:
+                    f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                        except Exception as e:
+                            logger.exception(e)
+                            f_caption=f_caption
+                    if f_caption is None:
+                        f_caption = f"{files.file_name}"
+   
                     k = await message.reply(f"⏳DOWNLOADING⏳◎ ◎")
                     await asyncio.sleep(1)
                     await k.delete()
