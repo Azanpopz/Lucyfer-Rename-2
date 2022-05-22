38b3dcb2deb119d82b1dee96d27a5088783e5be4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-22 12:59:52 +0530

    Update start.py

--- plugins/Rename/start.py
+++ plugins/Rename/start.py
@@ -6,7 +6,7 @@ SUPPORT_CH = 'Crazebots'
 YOUTUBE = 'TechnologyRk'
 
 
-@Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
+@Client.on_message(filters.private filters.command (["rename"]) &( filters.document | filters.audio | filters.video ))
 async def send_doc(client,message):
        media = await client.get_messages(message.chat.id,message.message_id)
        file = media.document or media.video or media.audio 
