e5ab3929d7d2cbb64258dc40dfadd7e0770fc939
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-22 13:46:31 +0530

    Update start.py

--- plugins/Rename/start.py
+++ plugins/Rename/start.py
@@ -22,17 +22,17 @@ async def filter(bot, update):
 
 
 @Client.on_message(filters.private & filters.command(["rename"]))
-async def send_doc(client, message):
-	if (message.chat.id,message.message_id)
+async def send_doc(client, message)
+  if (message.chat.id,message.message_id)
    try:
-			message = await message.reply("Converting...")
-                        media = await client.get_messages(message.chat.id,message.message_id)
-                        file = media.document or media.video or media.audio 
-                        filename = file.file_name
-                        filesize = humanize.naturalsize(file.file_size)
-                        fileid = file.file_id
-                        await message.reply_text(
-                        f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}"""
-                        ,reply_to_message_id = message.message_id,
-                        reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
-                        ,InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
+
+       media = await client.get_messages(message.chat.id,message.message_id)
+       file = media.document or media.video or media.audio 
+       filename = file.file_name
+       filesize = humanize.naturalsize(file.file_size)
+       fileid = file.file_id
+       await message.reply_text(
+       f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}"""
+       ,reply_to_message_id = message.message_id,
+       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
+       ,InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
