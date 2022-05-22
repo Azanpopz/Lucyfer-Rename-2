2e860e42953fcbfb1bcc8c3345f90967264064e9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-22 12:22:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,6 +20,33 @@ from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 
+
+
+from pyrogram import Client
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+
+APP_ID = int(os.environ.get("APP_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+if __name__ == "__main__" :
+    plugins = dict(
+        root="plugins"
+    )
+    app = Client(
+        "renamer",
+        bot_token=TOKEN,
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        plugins=plugins
+    )
+
+
+
+
+
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
