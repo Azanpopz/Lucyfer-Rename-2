8957af852c830af1ae3f1745063c3a249ae135b1
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-22 14:06:47 +0530

    Update start.py

--- plugins/Rename/start.py
+++ plugins/Rename/start.py
@@ -36,21 +36,14 @@ async def filter(bot, update):
 
 
 @Client.on_message(filters.private & filters.command(["rename"]))
-async def rename(client, message)
-    user_id = message.from_user.id
-    await message.reply_text("Checking Sticker")
-    
-    if message.reply_to_message.media is False:
-        await message.reply_text("Not a Sticker File!!")
-                   try :     
-                        user_id = message.from_user.id
-                        media = await client.get_messages(message.chat.id,message.message_id)
-                        file = media.document or media.video or media.audio 
-                        filename = file.file_name
-                        filesize = humanize.naturalsize(file.file_size)
-                        fileid = file.file_id
-                        await message.reply_text(
-                        f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}"""
-                        ,reply_to_message_id = message.message_id,
-                        reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
-                        ,InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
+async def rename(client,message):
+       media = await client.get_messages(message.chat.id,message.message_id)
+       file = media.document or media.video or media.audio 
+       filename = file.file_name
+       filesize = humanize.naturalsize(file.file_size)
+       fileid = file.file_id
+       await message.reply_text(
+       f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}"""
+       ,reply_to_message_id = message.message_id,
+       reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename ",callback_data = "rename")
+       ,InlineKeyboardButton("Cancel✖️",callback_data = "cancel")  ]]))
