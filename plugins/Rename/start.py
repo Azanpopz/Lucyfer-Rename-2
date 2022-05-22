347a1a6bfa30a702056935ee92679876171e9ceb
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-05-22 19:16:45 +0530

    Update start.py

--- plugins/Rename/start.py
+++ plugins/Rename/start.py
@@ -4,18 +4,6 @@ import humanize
 from helper.database import  insert 
 SUPPORT_CH = 'Crazebots'
 YOUTUBE = 'TechnologyRk'
-@Client.on_message(filters.private & filters.command(["start"]))
-async def start(client,message):
-	insert(int(message.chat.id))
-	await message.reply_text(text =f"""
-	Hello {message.from_user.first_name }
-	__I am file renamer bot, Please sent any telegram 
-	**Document Or Video** and enter new filenameto rename it__
-	""",reply_to_message_id = message.message_id ,  
-	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url=f"https://t.me/{SUPPORT_CH}") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url=f"https://youtube.com/c/{YOUTUBE}") ]  ]))
-
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client,message):
